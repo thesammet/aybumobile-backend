a2b01ab50724b9e2ed555a0ba2e12523cb8117ee
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2022-11-26 01:45:53 +0300

    addition: trends

--- src/routers/food.js
+++ src/routers/food.js
@@ -5,6 +5,8 @@ const admin = require('../middleware/admin')
 const Food = require('../models/food')
 const foodFetch = require('../utils/food_fetch')
 const Rating = require('../models/rating')
+const dynamicSort = require('../utils/dynamic_sort')
+const _ = require('lodash')
 
 router.post('/food', admin, auth, async (req, res) => {
     try {
@@ -29,6 +31,24 @@ router.get('/food', auth, async (req, res) => {
             foodSocialResult.push({ meal: element, social: { likes: likeCount, dislikes: dislikeCount } })
         }
         res.status(200).send({ data: foodSocialResult })
+    } catch (error) {
+        res.status(400).send({ error })
+    }
+})
+
+router.get('/trends', auth, async (req, res) => {
+    try {
+        const foods = await Food.find({})
+        let foodSocialResult = []
+        for await (const element of foods) {
+            const likeCount = (await Rating.find({ food: element._id, rating: 'like' })).length
+            const dislikeCount = (await Rating.find({ food: element._id, rating: 'dislike' })).length
+            foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v"]), comments: element.commentCount, likes: likeCount, dislikes: dislikeCount })
+        }
+        const mostCommentFood = foodSocialResult.sort(dynamicSort("-comments")).slice(0, 10)
+        const likeTrend = foodSocialResult.sort(dynamicSort("-likes")).slice(0, 10)
+        const dislikeTrend = foodSocialResult.sort(dynamicSort("-dislikes")).slice(0, 10)
+        res.status(200).send({ data: { commentTrend: mostCommentFood, likeTrend: likeTrend, dislikeTrend: dislikeTrend } })
     } catch (error) {
         res.status(400).send({ error })
     }
