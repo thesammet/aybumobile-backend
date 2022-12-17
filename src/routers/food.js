5383b421ae2e08cd709dd1cf29d5a01dacec2204
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2022-12-17 08:00:38 +0300

    Update food.js

--- src/routers/food.js
+++ src/routers/food.js
@@ -43,9 +43,9 @@ router.get('/food', auth, async (req, res) => {
         for await (const element of foods) {
             const ratingStatus = await Rating.find({ food: element._id, owner: req.user._id })
             if (ratingStatus.length != 0) {
-                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: likeCount, dislikes: dislikeCount, ratingStatus: ratingStatus[0].rating })
+                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: element.likeCount, dislikes: element.dislikeCount, ratingStatus: ratingStatus[0].rating })
             } else
-                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: likeCount, dislikes: dislikeCount, ratingStatus: 'inactive' })
+                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: element.likeCount, dislikes: element.dislikeCount, ratingStatus: 'inactive' })
         }
         res.status(200).send({ data: foodSocialResult })
     } catch (error) {
@@ -68,9 +68,9 @@ router.get('/trend', auth, async (req, res) => {
         for await (const element of foods) {
             const ratingStatus = await Rating.find({ food: element._id, owner: req.user._id })
             if (ratingStatus.length != 0) {
-                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: likeCount, dislikes: dislikeCount, ratingStatus: ratingStatus[0].rating })
+                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: element.likeCount, dislikes: element.dislikeCount, ratingStatus: ratingStatus[0].rating })
             } else
-                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: likeCount, dislikes: dislikeCount, ratingStatus: 'inactive' })
+                foodSocialResult.push({ meal: _.omit(element.toObject(), ["commentCount", "__v", "epoch"]), comments: element.commentCount, likes: element.likeCount, dislikes: element.dislikeCount, ratingStatus: 'inactive' })
         }
         const commentTrend = foodSocialResult.sort(dynamicSort("-comments")).slice(0, 7)
         const likeTrend = foodSocialResult.sort(dynamicSort("-likes")).slice(0, 7)
