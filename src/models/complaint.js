727bb07dfc5cfdcdcd2aa50cc288a6fd1d289e21
thesammet <thesammet@gmail.com>
2023-09-30 19:46:49 +0300

    Update complaint.js

--- src/models/complaint.js
+++ src/models/complaint.js
@@ -18,11 +18,9 @@ const complaintSchema = new mongoose.Schema({
     },
     title: {
         type: String,
-        required: true
     },
     description: {
         type: String,
-        required: true
     }
 },
     {
